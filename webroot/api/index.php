60fa860c8dd00922464c806f0e993d783e1b1386
tbnovak <tylerbnovak@gmail.com>
2020-04-16 17:17:09 -0500

    Go to PHP for a text response

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -3,5 +3,6 @@
  * This is a template php file for your countries search.
  * Use as you will, or start over. It's up to you.
  */
- header('Content-Type: application/json');
- echo json_encode(['data' => ['Your data']]);
+//header('Content-Type: application/json');
+echo "#YOLO";
+?>
--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,6 +1,14 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
-function searchFunction(){
+function searchFunction() {
 	var searchString = document.getElementById("searchText").value;
-	alert(searchString);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+   			document.getElementById("result").innerHTML=this.responseText;
+        }
+    };
+    xmlhttp.open("GET", "http://localhost:8765/api/index.php?q=" + searchString, true);
+    xmlhttp.send();
+	return false;
 }
