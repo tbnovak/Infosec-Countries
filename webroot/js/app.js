4e51c9652547c09ab3c42ecfc7b90e15cff328a5
tbnovak <tylerbnovak@gmail.com>
2020-04-16 11:38:14 -0500

    HTML form. Javascript successfully invoked.

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,2 +1,5 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
+function searchFunction(){
+	alert("searchFunction");
+}
