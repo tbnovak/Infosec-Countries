f963d20bf348d391decf1092b2afeb028b2b5140
tbnovak <tylerbnovak@gmail.com>
2020-04-16 11:43:07 -0500

    Get the search string in the javascipt function

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,5 +1,6 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
 function searchFunction(){
-	alert("searchFunction");
+	var searchString = document.getElementById("searchText").value;
+	alert(searchString);
 }
