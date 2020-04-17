f5eccfcf66d0076a65bf47cf2ee150b63fec66a0
tbnovak <tylerbnovak@gmail.com>
2020-04-17 09:35:48 -0500

    wrap the php query in a function. Still dummy, but it won't be for long

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -3,11 +3,13 @@
  * This is a template php file for your countries search.
  * Use as you will, or start over. It's up to you.
  */
+function queryCountries($searchString){
+	$result->search = $searchString;
+	$result->result = array("YO",$searchString,"LO");
+	return $result;
+}
 //header('Content-Type: application/json');
 $obj = json_decode($_POST["q"],false);
-$searchString=$obj->searchString;
-$result->search = $searchString;
-$result->result=array("YO",$searchString,"LO");
-echo json_encode($result);
-//echo "#YO ".$obj->searchString.$obj->searchString." LO";
+$searchString = $obj->searchString;
+echo json_encode(queryCountries($searchString));
 ?>
