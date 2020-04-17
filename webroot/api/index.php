223e28ff7bdee2853eaa88671479a7345440f0b7
tbnovak <tylerbnovak@gmail.com>
2020-04-17 10:12:10 -0500

    Hitting the Country API

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -4,12 +4,13 @@
  * Use as you will, or start over. It's up to you.
  */
 function queryCountries($searchString){
-	$result->search = $searchString;
-	$result->result = array("YO",$searchString,"LO");
+	$url = "https://restcountries.eu/rest/v2/alpha/".$searchString;
+	$result = file_get_contents($url);
 	return $result;
 }
-//header('Content-Type: application/json');
+header('Content-Type: application/json');
 $obj = json_decode($_POST["q"],false);
 $searchString = $obj->searchString;
-echo json_encode(queryCountries($searchString));
+//echo json_encode(queryCountries($searchString));
+echo queryCountries($searchString);
 ?>
--- webroot/js/app.js
+++ webroot/js/app.js
@@ -8,7 +8,8 @@ function searchFunction() {
         if (this.readyState == 4 && this.status == 200) {
    			//document.getElementById("result").innerHTML=this.responseText;
    			var result = JSON.parse(this.responseText);
-   			document.getElementById("result").innerHTML = result.result[0] + result.result[1] + result.result[2]
+   			//document.getElementById("result").innerHTML = result.result[0] + result.result[1] + result.result[2]
+			document.getElementById("result").innerHTML = result.name;
         }
     };
     xmlhttp.open("POST", "http://localhost:8765/api/index.php", true);
