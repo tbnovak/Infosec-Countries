b0a985f796ee923eb2baf0a51b6bc5fdab2f2e69
tbnovak <tylerbnovak@gmail.com>
2020-04-17 15:11:16 -0500

    rough display table

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -8,17 +8,38 @@ function searchFunction() {
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
    			var resTxt = "";
+			var newBody = document.createElement('tbody');
         	if(this.responseText && this.responseText != ""){
 	   			var result = JSON.parse(this.responseText);
 	   			for (country in result){
 	   				resTxt+=result[country].name+"<br>";
+	   				addRow(newBody,result[country]);
 	   			}
    			}
-   			document.getElementById("result").innerHTML = (resTxt ? resTxt : "No results");
+   			var oldBody = document.getElementById("tblResult").tBodies[0];
+   			oldBody.parentNode.replaceChild(newBody,oldBody);
+   			document.getElementById("result").innerHTML = (resTxt ? "" : "No results");
         }
     };
     xmlhttp.open("POST", "http://localhost:8765/api/index.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("q="+data);
 	return false;
+}
+function languageArrayToString(array){
+	var langs = [];
+	for (lang of array){
+		langs.push(lang.name);
+	}
+	return langs.join(", ");
+}
+function addRow(body,country){
+	var row = body.insertRow(body.length);
+	row.insertCell(0).innerHTML = country.name;
+	row.insertCell(1).innerHTML = country.alpha3Code;
+	row.insertCell(2).innerHTML = country.flag;
+	row.insertCell(3).innerHTML = country.region;
+	row.insertCell(4).innerHTML = country.subregion;
+	row.insertCell(5).innerHTML = country.population;
+	row.insertCell(6).innerHTML = languageArrayToString(country.languages);
 }
