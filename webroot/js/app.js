5f47283c6eb643fe173d6322b1c8c053fc0aa467
tbnovak <tylerbnovak@gmail.com>
2020-04-20 08:54:20 -0500

    query's status update function is no longer anonymous. Just style

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -19,27 +19,28 @@ function initPage(){
 	appendCol(table).classList += "langs";
 	appendHeaderCell(hrow).innerHTML = "Languages";
 }
+function queryStatueChange() {
+    if (this.readyState == 4 && this.status == 200) {
+			var resTxt = "";
+		var newBody = document.createElement('tbody');
+    	if(this.responseText && this.responseText != ""){
+   			var result = JSON.parse(this.responseText);
+   			for (country in result){
+   				resTxt+=result[country].name+"<br>";
+   				addRow(newBody,result[country]);
+   			}
+			}
+			var oldBody = document.getElementById("tblResult").tBodies[0];
+			oldBody.parentNode.replaceChild(newBody,oldBody);
+			document.getElementById("result").innerHTML = (resTxt ? "" : "No results");
+    }
+}
 function searchFunction() {
 	var searchString = document.getElementById("searchText").value;
 	var mode = document.querySelector('input[name = "mode"]:checked').value;
 	var data = JSON.stringify({"searchString": searchString, "mode": mode});
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-   			var resTxt = "";
-			var newBody = document.createElement('tbody');
-        	if(this.responseText && this.responseText != ""){
-	   			var result = JSON.parse(this.responseText);
-	   			for (country in result){
-	   				resTxt+=result[country].name+"<br>";
-	   				addRow(newBody,result[country]);
-	   			}
-   			}
-   			var oldBody = document.getElementById("tblResult").tBodies[0];
-   			oldBody.parentNode.replaceChild(newBody,oldBody);
-   			document.getElementById("result").innerHTML = (resTxt ? "" : "No results");
-        }
-    };
+    xmlhttp.onreadystatechange = queryStatueChange;
     xmlhttp.open("POST", "http://localhost:8765/api/index.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("q="+data);
